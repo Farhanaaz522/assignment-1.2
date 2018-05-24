b9ca325420f137de0078f741cfe3505d712d7a04
Farhanaaz522 <37798788+Farhanaaz522@users.noreply.github.com>
2018-05-24 22:09:13 +0530

    Update assignment 1.2.R

--- assignment 1.2.R
+++ assignment 1.2.R
@@ -7,34 +7,25 @@ t =c(8, 3, 4)
 print(v%/%t)
 #answer
 0 1 1
-#2. You have 25 excel files with names as xx_1.xlsx, xx_2.xlsx,........xx_25.xlsx 
-#   in a dir.
-#   Write a program to extract the contents of each excel sheet and make it 
-#   one df.
-
-#Answer 2
-setwd("C:\\Users\\mudassar\\Desktop\\farha")
-library('readxl')
-for (i in 1:25) 
-  {
-  a=paste("xx_",i,sep="")
-  a=paste(a,".xlsx",sep ="")
-  df= read_xlsx(a)
-  b=rbind(b,df)
- 
-}
 
 
-#3. If the above 25 files were csv files, what would be your script to read?
+#Question 2.You have 25 excel files with names as xx_1.xlsx, xx_2.xlsx,……..xx_25.xlsx in a dir. 
+#Write a program to extract the contents of each excel sheet and make it one df. 
 
-#Answer 3
+#Answer: 
+Setwd(“c:path of the folder”)
+File.list=list.files(pattern=’*.xlsx’)
+Df.list=lapply(file.list, read_excel)
+Df1=rbindlist(df.list,idcol=”id”)
+View(df1)
 
-#use for loop
-for (i in 1:25)
-  {
-  name=paste("xx_",i,sep="")
-  name=paste(name,".csv",sep ="")
-  df= read.csv(name)
-  b=rbind(b,df) 
-}
+
+#Question 3. If the above 25 files were csv files, what would be your script to read?
+#Answer: 
+Setwd(“c:path of the folder”)
+File.list<-list.files(pattern=’*.csv’)
+Df.list<-lapply(file.list, read.csv)
+Df1<-rbindlist(df.list,idcol=”id”)
+View(df1)
+
 
